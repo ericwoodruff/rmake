bc194b263ee3002016afacab752348aad811b347
Eric Woodruff <eric.woodruff@gmail.com>
2012-04-30 14:27:27 -0700

    pull passwd

--- bin/mkchroot.sh
+++ bin/mkchroot.sh
@@ -189,7 +189,7 @@ function mkchroot-init () {
 
 	rpm --initdb --root $root
 	#yum groupinstall "Core"
-	yum install which yum bash rsync openssh-server $packages
+	yum install passwd which yum bash rsync openssh-server $packages
 
 	cat <<-"EOF" >> $root/root/.bash_profile
 	if [ -f ~/.bashrc ]; then
